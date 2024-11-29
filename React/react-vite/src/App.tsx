6af799984a5119fbb2a1d920e231dc6b230ec0ff
UchithmaSenevirathne <uchithma@gmail.com>
2024-11-29 11:56:40 +0530

    components

--- React/react-vite/src/App.tsx
+++ React/react-vite/src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+// import ButtonComponent from "./component/ButtonComponent.tsx";
+import {Item} from "./component/Item.tsx";
+import {ItemList} from "./Items.ts";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    // =======04==============
+    const itemArray = [];
+
+    for (const item of ItemList) {
+        itemArray.push(<Item title={item.title} description={item.description} />);
+    }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        {/*=========04===========*/}
+        {itemArray}
+
     </>
   )
 }
 
 export default App
+
+{/*<ButtonComponent name="Add"></ButtonComponent>*/}
+{/*<ButtonComponent name="Delete"></ButtonComponent>*/}
+{/*<ButtonComponent name="Update"></ButtonComponent>*/}
+
+{/*=====01=======*/}
+
+{/*<Item title={ItemList[0].title} description={ItemList[0].description} />*/}
+{/*<Item title={ItemList[1].title} description={ItemList[1].description} />*/}
+{/*<Item title={ItemList[2].title} description={ItemList[2].description} />*/}
+
+{/*=====02=========*/}
+
+{/*<Item {...ItemList[0]}/>*/}
+{/*<Item {...ItemList[1]}/>*/}
+{/*<Item {...ItemList[2]}/>*/}
+
+{/*=========03==========*/}
+
+{/*{ItemList.map((item) => (*/}
+{/*    <Item title={item.title} description={item.description} />*/}
+{/*))}*/}
---
+++ React/react-vite/src/Items.ts
@@ -0,0 +1,14 @@
+export const ItemList = [
+    {
+        title:"A",
+        description:"this is a description",
+    },
+    {
+        title:"B",
+        description:"this is a description",
+    },
+    {
+        title:"C",
+        description:"this is a description",
+    }
+]
